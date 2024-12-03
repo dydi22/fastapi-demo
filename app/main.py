c7314832061b5162ae1078fc3ba26f7d2ca80c19
Dylan Dietrich <157732045+dydi22@users.noreply.github.com>
2024-12-03 15:23:39 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python3
 
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-import mysql.connector
-from mysql.connector import Error
 import json
 import os
-
-
-DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
-DBUSER = "ds2022"
-DBPASS = os.getenv('DBPASS')
-DB = "atv7xh"
-
-# db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
-# cur=db.cursor()
+from fastapi import FastAPI
+import mysql.connector
+from mysql.connector import Error
 
 app = FastAPI()
-
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
@@ -26,48 +14,53 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-def get_db_connection():
-    db = mysql.connector.connect(
-        host=DBHOST,
-        user=DBUSER,
-        password=DBPASS,
-        database=DB
-    )
-    cursor = db.cursor(dictionary=True)  # Fetch rows as dictionaries
-    return db, cursor
+
+DBHOST = "ds2022.cqee4iwdcaph.us-east-1.rds.amazonaws.com"
+DBUSER = "ds2022"
+DBPASS = os.getenv('DBPASS')
+DB = "atv7xh"
+
+@app.get("/")  # zone apex
+def zone_apex():
+    return {"Good Day": "Sunshine!"}
 
 @app.get('/genres')
 async def get_genres():
-    # db.cmd_refresh(refresh)
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
     cur = db.cursor()
     query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
-        db, cur = get_db_connection()  # Ensure this function is correctly implemented
         cur.execute(query)
-        results = cur.fetchall()  # Fetch all rows
-        return {"genres": results}  # Return the results as JSON
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
     except Error as e:
         print("MySQL Error: ", str(e))
+        return {"Error": "MySQL Error: " + str(e)}
+    finally:
         cur.close()
         db.close()
-        return {"Error": "MySQL Error: " + str(e)}
 
 @app.get('/songs')
 async def get_genres():
-    # db.cmd_refresh(refresh)
-    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB, ssl_disabled=True)
     cur = db.cursor()
-    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid;"
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres WHERE songs.genre = genres.genreid ORDER BY songs.title;"
     try:
         cur.execute(query)
-        results = cur.fetchall()  # Fetch all rows as dictionaries
-        return{"songs": results}
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        return(json_data)
     except Error as e:
-        return{"Error": f"MySQL Error: {str(e)}"}
+        print("MySQL Error: ", str(e))
+        return None
     finally:
-        if 'cur' in locals() and cur:
-            cur.close()
-        if 'db' in locals() and db.is_connected():
-            db.close()
+        cur.close()
+        db.close()
 
